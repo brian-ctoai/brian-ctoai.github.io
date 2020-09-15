b9e51519355a2ac8baae05297413e466d964eb78
Brian St. Amand <brian@cto.ai>
2020-09-15 04:19:31 -0700

    update

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const config = jQuery.ajax("config.json");
-const URL = config.CTOAI_API_URL;
-
-if (URL === "https://api.cto.ai") {
-  document.getElementsByClassName("broken")[0].style.visibility = "hidden";
-} else {
-  document.getElementsByClassName("working")[0].style.visibility = "hidden";
-}
+jQuery.ajax("config.json").then(config => {
+		if (config.CTOAI_API_URL === "https://api.cto.ai") {
+			document.getElementsByClassName("broken")[0].style.visibility = "hidden";
+		} else {
+			document.getElementsByClassName("working")[0].style.visibility = "hidden";
+		}
+	}
+);
